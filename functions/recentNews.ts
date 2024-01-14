a02992036e51ed558b1eb5a97b3d40ed06835400
KShervington <kyle.shervington@gmail.com>
2024-01-14 16:56:09 -0500

    getting article content as expected

--- functions/recentNews.ts
+++ functions/recentNews.ts
@@ -16,6 +16,7 @@ const newsSources = [
 
 const articles: Array<Object> = [];
 
+// returns the css selector used for article titles on the respective websites
 function ChooseTitles(source: string) {
 
     switch (source) {
@@ -34,6 +35,24 @@ function ChooseTitles(source: string) {
 
 }
 
+async function getArticleContent(url: string) {
+
+    let bodyContent: string = "";
+
+    await axios.get(url)
+        .then(response => {
+            const html = response.data;
+            const $ = cheerio.load(html);
+
+            $(".meat > p", html).each(function () {
+                bodyContent += $(this).text();
+            })
+
+        })
+
+    return bodyContent;
+}
+
 const getLatestNews = async (sourceId: string, numArticles: number) => {
 
     const sourceAddress = newsSources.filter(source => source.name == sourceId)[0].address;
@@ -43,36 +62,60 @@ const getLatestNews = async (sourceId: string, numArticles: number) => {
 
     try {
         await axios.get(sourceAddress)
-            .then(response => {
+            .then(async response => {
                 const html = response.data;
                 const $ = cheerio.load(html);
-                var articleTitleHTML: string, title: string, url: string | undefined;
+                var articleTitleSelector: string;
 
-                articleTitleHTML = ChooseTitles(sourceId);
+                articleTitleSelector = ChooseTitles(sourceId);
 
-                // for (let i = 0; i < numArticles; i++) {
-                //     let currArticle = $(articleTitleHTML).next();
+                // NEW new way to push to article object
+                for (let i = 0; i < numArticles; i++) {
 
-                //     title = currArticle.text();
-                //     url = currArticle.attr('href');
+                    // '.eq()' is used to select specific index | https://cheerio.js.org/docs/basics/traversing#eq
+                    const title = $(articleTitleSelector, html).eq(i).text();
+                    const endpoint = $(articleTitleSelector, html).eq(i).attr('href');
 
-                //     singleArticles.push({
-                //         title,
-                //         url: sourceBase + url,
-                //         source: sourceId
-                //     })
-                // }
-
-                $(articleTitleHTML, html).each(function () {
-                    const title = $(this).text();
-                    const url = $(this).attr('href');
+                    const content: string = await getArticleContent(sourceBase + endpoint);
 
                     singleArticles.push({
                         title,
-                        url: sourceBase + url,
-                        source: sourceId
+                        url: sourceBase + endpoint,
+                        source: sourceId,
+                        body: content
                     })
-                });
+                }
+
+                // NEW way to push to article object
+                // for (const article of $(articleTitleSelector, html)) {
+                //     const title = $(article).text();
+                //     const endpoint = $(article).attr('href');
+
+                //     const content: string = await getArticleContent(sourceBase + endpoint);
+
+                //     singleArticles.push({
+                //         title,
+                //         url: sourceBase + endpoint,
+                //         source: sourceId,
+                //         body: content
+                //     })
+                // }
+
+                // OLD way to push to article object
+                // $(articleTitleSelector, html).each(function () {
+                //     const title = $(this).text();
+                //     const endpoint = $(this).attr('href');
+
+                //     // TODO: need to figure out how to populate this var
+                //     const content: string = await getArticleContent(sourceBase + endpoint);
+
+                //     singleArticles.push({
+                //         title,
+                //         url: sourceBase + endpoint,
+                //         source: sourceId,
+                //         body: content
+                //     })
+                // });
 
             })
     }
@@ -81,7 +124,7 @@ const getLatestNews = async (sourceId: string, numArticles: number) => {
     }
     finally {
 
-        return singleArticles.slice(0, numArticles);
+        return singleArticles;
     }
 }
 
@@ -93,7 +136,13 @@ interface ConsolidateNewsParams {
 // This function will organize full articles for Chat GPT to summarize
 export const consolidateNews = async (params: ConsolidateNewsParams) => {
 
-    const topNews: Array<Object> = await getLatestNews(params.source, params.numArticles);
+    const recentNews: Array<Object> = await getLatestNews(params.source, params.numArticles);
 
-    return topNews;
+    return recentNews;
 };
+
+const main = async () => {
+    const body: string = await getArticleContent("https://www.animenewsnetwork.com//news/2024-01-13/sanrio-wanpaku-touken-ranbu-project-has-anime-in-the-works/.206437");
+
+    console.log(body === undefined ? "\'body\' is undefined" : body);
+}
